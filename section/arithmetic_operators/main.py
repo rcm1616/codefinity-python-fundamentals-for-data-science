4eef922d2de528ae5d77b9c4185bc7122c450951
rcm1616 <>
2026-03-24 16:36:02 +0000

    Arithmetic Operators

---
+++ section/arithmetic_operators/main.py
@@ -0,0 +1,11 @@
+# Calculate addition
+print(5.42 + 4.39) 
+
+# Calculate subtraction
+print(12.30 - 5.50)  
+
+# Calculate multiplication
+print(7.32 * 0.2) 
+
+# Calculate division
+print(8.30 / 20)
